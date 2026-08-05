8ae9bd5be09837c84006afb3c7915dea7349c5cd
marcogatti1996-hub <marcogatti1996@gmail.com>
2026-08-05 11:21:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ button.addEventListener("click", () => {
     envelope.style.display = "none";
     invitation.classList.remove("hidden");
 });
+
+const weddingDate = new Date("October 10, 2027 00:00:00").getTime();
+
+setInterval(() => {
+
+    const now = new Date().getTime();
+    const distance = weddingDate - now;
+
+    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days;
+    document.getElementById("hours").textContent = hours;
+    document.getElementById("minutes").textContent = minutes;
+    document.getElementById("seconds").textContent = seconds;
+
+}, 1000);
